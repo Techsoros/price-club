02f970e206862446f119ecf7d3d491481952b66e
Your Name~ <f.zihad333@gmail.com>
2022-10-09 20:19:31 +0600

    Responsive header added

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Link = ({ value }) => {
   return (
-    <li>
-      <a href={value.path}>{value.name}</a>
+    <li className=" border-b-2 md:border-b-0  border-blue-400 mx-10 md:mx-0">
+      <a href={value.path} className="">
+        {value.name}
+      </a>
     </li>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import Link from "../Link/Link";
-import {
-  BeakerIcon,
-  Bars3BottomLeftIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/solid";
+import { Bars3BottomLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const links = [
     { id: 1, name: "Home", path: "/home" },
     { id: 2, name: "Products", path: "/Products" },
@@ -16,20 +12,22 @@ const Navbar = () => {
     { id: 5, name: "Contact", path: "/Contact" },
   ];
   return (
-    <nav>
-      {open ? (
-        <XMarkIcon
-          onClick={() => setOpen(!open)}
-          className="h-10 w-10 cursor-pointer text-gray-600"
-        />
-      ) : (
-        <Bars3BottomLeftIcon
-          onClick={() => setOpen(!open)}
-          className="h-10 w-10 cursor-pointer text-gray-600"
-        />
-      )}
+    <nav className="md:py-5  bg-slate-300 ">
+      <div
+        className="toggle-icon h-10 w-10 block md:hidden  z-10  cursor-pointer text-gray-600"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? (
+          <XMarkIcon></XMarkIcon>
+        ) : (
+          <Bars3BottomLeftIcon></Bars3BottomLeftIcon>
+        )}
+      </div>
 
-      <ul className="flex flex-col md:flex-row  justify-center gap-5 md:">
+      <ul
+        className={`flex flex-col md:flex-row w-full duration-1000 ease-out absolute md:static justify-center gap-5  bg-slate-300 
+        ${open ? "top-10" : "top-[-1200px] "}`}
+      >
         {links.map((link) => (
           <Link key={link.id} value={link}></Link>
         ))}
